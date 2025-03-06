38113a31afc31df717d7553455289d4f715603bb
Andreev Alexey <andreev-j@yandex.ru>
2025-03-06 15:47:21 +0300

    add homework09

---
+++ hw09/src/main/java/ru/otus/demootusmemorydump/OtusVisualVmApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.demootusmemorydump;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtusVisualVmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtusVisualVmApplication.class, args);
+    }
+
+}
---
+++ hw09/src/main/resources/db/migration/V1_1__create_security_tables.sql
@@ -0,0 +1,6 @@
+
+create table public.users (
+                                 id bigserial,
+                                 user_name varchar(100),
+                                 user_pass varchar(1000)
+);

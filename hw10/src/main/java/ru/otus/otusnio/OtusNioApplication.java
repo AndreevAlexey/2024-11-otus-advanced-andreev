606e5b6d5d2a2e6e36eae9a99c7bb4b422c90fcb
Andreev Alexey <andreev-j@yandex.ru>
2025-03-13 15:01:11 +0300

    add homework10 code

---
+++ hw10/src/main/java/ru/otus/otusnio/OtusNioApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.otusnio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtusNioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtusNioApplication.class, args);
+    }
+
+}
---
+++ hw10/src/test/java/ru/otus/otusnio/OtusNioApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.otusnio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtusNioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
